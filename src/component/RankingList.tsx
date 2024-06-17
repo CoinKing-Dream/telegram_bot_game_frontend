be6824db679ccadd91dfa0c156d8e52550b4f787
MitsuiMinoru <mitsuimino711@gamil.com>
2024-06-17 14:01:54 +1000

    U

--- src/component/RankingList.tsx
+++ src/component/RankingList.tsx
@@ -37,12 +37,12 @@ export default function RankingList() {
   }
   return (
     <div className="md:w-full h-[60vh] mx-auto ">
-        <div className="flex px-3 py-1 text-white text-lg max-sm:text-base font-bold justify-center align-middle overflow-y-hidden">
+        <div className="flex px-1 py-3 max-sm:py-1 text-white text-2xl max-sm:text-base font-bold justify-center align-middle overflow-y-hidden">
           <div className="text-start w-[20%] flex justify-center">Rank</div>
           <div className="text-start w-[60%] flex justify-center">User</div>
           <div className="text-start w-[20%] flex justify-center">RFP</div>
         </div>
-      <div className="max-h-[60vh] max-sm:max-h-[65vh] overflow-auto">
+      <div className="max-h-[52vh] max-sm:max-h-[65vh] overflow-auto">
         {users.map((data, index) => (
           
           <div
@@ -51,13 +51,13 @@ export default function RankingList() {
               index > 0 && "my-2"
             } px-3 max-sm:px-1 items-center bg-[#363636] rounded-lg`}
           >
-            <div className="text-xl max-sm:text-base text-start ml-3 pl-2 max-sm:ml-2 max-sm:pl-1 w-[20%] text-white flex justify-center align-middle">
+            <div className="text-2xl max-sm:text-base text-start ml-3 pl-2 max-sm:ml-2 max-sm:pl-1 w-[10%] text-white flex justify-center align-middle">
               {index == 0 ? (
-                <img src="image/crown.png" alt="" className="w-[30px] h-[25px] max-md:w-[20px] max-md:h-[20px] max-sm:w-[19px] max-sm:h-[20px]"/>
+                <img src="image/crown.png" alt="" className="w-[30px] h-[30px] max-md:w-[20px] max-md:h-[20px] max-sm:w-[19px] max-sm:h-[20px]"/>
               ) : index == 1 ? (
-                <img src="image/trophy.png" alt="" className="w-[30px] h-[25px] max-md:w-[20px] max-md:h-[20px] max-sm:w-[19px] max-sm:h-[20px]"/>
+                <img src="image/trophy.png" alt="" className="w-[30px] h-[30px] max-md:w-[20px] max-md:h-[20px] max-sm:w-[19px] max-sm:h-[20px]"/>
               ) : index == 2 ? (
-                <img src="image/star.png" alt=""  className="w-[30px] h-[25px] max-md:w-[20px] max-md:h-[20px] max-sm:w-[19px] max-sm:h-[20px]"/>
+                <img src="image/star.png" alt=""  className="w-[30px] h-[30px] max-md:w-[20px] max-md:h-[20px] max-sm:w-[19px] max-sm:h-[20px]"/>
               ) : (
                 index + 1
               )}
@@ -67,7 +67,7 @@ export default function RankingList() {
               <p className="text-xl max-sm:text-base text-start pl-2 text-white">{data.wallet_address}</p>
             </div>
 
-            <p className="text-2xl max-sm:text-xl text-start ml-12 pl-12 w-[30%] text-white max-sm:pl-4 max-sm:ml-2 ">
+            <p className="text-2xl max-sm:text-xl text-start ml-12 pl-12 w-[20%] text-white max-sm:pl-4 max-sm:ml-2 ">
               {formatNumberWithCommas(data.balance)}
             </p>
           </div>
@@ -75,9 +75,9 @@ export default function RankingList() {
       </div>
       <hr className="border-[#363636] border-2 max-sm:border-1" />
       <div
-        className={`flex my-3 max-sm:my-1 px-3 max-sm:px-1 items-center bg-[#5A4C3B] rounded-lg`}
+        className={`flex my-3 max-sm:my-1 px-1 max-sm:px-1 items-center bg-[#5A4C3B] rounded-lg`}
       >
-        <div className="text-xl max-sm:text-base text-start pl-12 max-sm:pl-8 w-[20%] text-white">
+        <div className="text-2xl max-sm:text-base text-start pl-12 max-sm:pl-8 w-[20%] text-white">
           {user.ranking}
         </div>
         <div className="relative h-10 max-md:h-9 max-sm:h-7 overflow-hidden w-[60%] flex ml-10 pl-12 items-center  max-sm:pl-1 max-sm:ml-2">
